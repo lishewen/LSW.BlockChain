f9a8e7ec70b9e2f83579d47d03b87869a8072618
算神 <lishewen@msn.com>
2019-10-26 16:37:42 +0800

    CardService

---
+++ LSW.BlockChain/Data/CardService.cs
@@ -0,0 +1,20 @@
+﻿using LSW.BlockChain.Models;
+using LSW.BlockChain.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LSW.BlockChain.Data
+{
+    public class CardService
+    {
+        private readonly ICardSalesRepository _repository;
+        public CardService(ICardSalesRepository repository) => _repository = repository;
+
+        public async Task CreateCard(Card card)
+        {
+            await _repository.CreateCard(card);
+        }
+    }
+}
--- LSW.BlockChain/Startup.cs
+++ LSW.BlockChain/Startup.cs
@@ -33,6 +33,7 @@ namespace LSW.BlockChain
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddScoped<CardService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
